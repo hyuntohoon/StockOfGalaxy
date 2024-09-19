212a56128f42b25e4a01c38545db12295b69c274
선예림 <syl1602@naver.com>
2024-09-19 10:52:27 +0900

    feat: #8. config 파일 refactor

--- backend/stock/src/main/java/com/sog/stock/global/config/KisWebSocketConfig.java
+++ backend/stock/src/main/java/com/sog/stock/global/config/KisWebSocketConfig.java
@@ -3,33 +3,30 @@ package com.sog.stock.global.config;
 import com.sog.stock.application.service.KisTokenService;
 import com.sog.stock.application.service.StockService;
 import com.sog.stock.global.websocket.PriceStockHandler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
+import org.springframework.web.reactive.socket.client.StandardWebSocketClient;
 import org.springframework.web.reactive.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.WebSocketConnectionManager;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-public class KisWebSocketConfig {
+@EnableWebSocket
+@RequiredArgsConstructor
+@Slf4j
+public class KisWebSocketConfig implements WebSocketConfigurer {
 
-    @Value("${kis.websocket.domain}")
-    private String kisWebSocketDomain;
+    private final PriceStockHandler priceStockHandler;
 
-    private final KisTokenService kisTokenService;
-    private final StockService stockService;
-
-    public KisWebSocketConfig(KisTokenService kisTokenService, StockService stockService) {
-        this.kisTokenService = kisTokenService;
-        this.stockService = stockService;
-    }
-
-    @Bean
-    public PriceStockHandler priceStockHandler() {
-        return new PriceStockHandler(stockService, kisTokenService);
-    }
-
-    @Bean
-    public WebSocketClient webSocketClient() {
-        return new ReactorNettyWebSocketClient();
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        // pricestockhandler를 추가
+        registry.addHandler(priceStockHandler, "/stock").setAllowedOrigins("*"); // endpoint와 CORS 설정
     }
 }
