6512910dadec5980a6c8b1c02d66d520dd2524ac
박지훈 <zlxlgus123@naver.com>
2024-09-19 17:41:34 +0900

    feat #7 : NewsController 매개변수 추가

--- backend/news/src/main/java/com/sog/news/presentation/controller/NewsController.java
+++ backend/news/src/main/java/com/sog/news/presentation/controller/NewsController.java
@@ -1,7 +1,12 @@
 package com.sog.news.presentation.controller;
 
+import com.sog.news.application.service.NewsService;
+import com.sog.news.domain.dto.TodayNewsResponseDTO;
+import com.sog.news.domain.dto.TodayPlanetNewsResposeDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,16 +21,19 @@ import java.time.LocalDate;
 @Tag(name = "News", description = "뉴스 관련 API")
 public class NewsController {
 
+    @Autowired
+    private NewsService newsService;
+
     @Operation(summary = "오늘의 소식 조회", description = "오늘 날짜의 모든 뉴스를 조회합니다.")
     @GetMapping("/today")
-    public ResponseEntity<?> getTodayNews() {
-        return newsService.getTodayNews();
+    public ResponseEntity<List<TodayNewsResponseDTO>> getTodayNews(@RequestParam LocalDate date) {
+        return newsService.getTodayNews(date);
     }
 
     @Operation(summary = "오늘의 행성 뉴스 조회", description = "오늘 날짜의 행성 관련 뉴스를 조회합니다.")
     @GetMapping("/today/planet")
-    public ResponseEntity<?> getTodayPlanetNews() {
-        return newsService.getTodayPlanetNews();
+    public ResponseEntity<List<TodayPlanetNewsResposeDTO>> getTodayPlanetNews(@RequestParam LocalDate date, @RequestParam String stockCode) {
+        return newsService.getTodayPlanetNews(date, stockCode);
     }
 
     @Operation(summary = "뉴스 제목 키워드 검색", description = "뉴스 제목에서 키워드로 검색합니다.")
@@ -35,15 +43,15 @@ public class NewsController {
     }
 
     @Operation(summary = "날짜 주식별 뉴스 키워드 검색", description = "해당 날짜, 주식별 모든 키워드를 검색합니다.")
-    @GetMapping("/keyword-frequency")
-    public ResponseEntity<?> getKeywordFrequency(@RequestParam LocalDate date, @RequestParam(required = false) String stockCode) {
-        return newsService.getKeywordFrequency(date, stockCode);
+    @GetMapping("/keyword-frequency/daily-stock")
+    public ResponseEntity<?> getDailyStockKeywordFrequency(@RequestParam LocalDate date, @RequestParam(required = false) String stockCode) {
+        return newsService.getDailyStockKeywordFrequency(date, stockCode);
     }
 
     @Operation(summary = "날짜별 키워드 빈도수 조회", description = "해당 날짜의 모든 뉴스 기사의 키워드 검색합니다.")
     @GetMapping("/keyword-frequency/daily")
-    public ResponseEntity<?> getDailyKeywordFrequency(@RequestParam LocalDate startDate, @RequestParam LocalDate endDate) {
-        return newsService.getDailyKeywordFrequency(startDate, endDate);
+    public ResponseEntity<?> getDailyKeywordFrequency(@RequestParam LocalDate startDate) {
+        return newsService.getDailyKeywordFrequency(startDate);
     }
 
     @Operation(summary = "뉴스 검색", description = "뉴스를 상세 조회합니다.")
