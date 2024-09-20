d925a9c8671a527b26914ea372cad969b3a38665
박지훈 <zlxlgus123@naver.com>
2024-09-20 17:12:29 +0900

    fix #10 : Actuator 추가

--- backend/gateway/src/main/java/com/sog/gateway/global/LoggingFilterConfig.java
+++ backend/gateway/src/main/java/com/sog/gateway/global/LoggingFilterConfig.java
@@ -2,6 +2,10 @@ package com.sog.gateway.global;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class LoggingFilterConfig {
