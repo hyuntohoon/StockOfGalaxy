c87bb0f12b9790c5d07a5d1854044774a1d945db
선예림 <syl1602@naver.com>
2024-09-22 18:38:12 +0900

    feat: #12. 일변 과날짜별 과거 주식 정보 조회 필요 dto 작성

--- backend/stock/src/main/java/com/sog/stock/domain/dto/StockDailyPriceListResponseDTO.java
+++ backend/stock/src/main/java/com/sog/stock/domain/dto/StockDailyPriceListResponseDTO.java
@@ -1,4 +1,11 @@
 package com.sog.stock.domain.dto;
 
+import java.util.List;
+import lombok.Data;
+
+@Data
 public class StockDailyPriceListResponseDTO {
+
+    private List<StockDailyPriceResponseDTO> stockDailyPriceList;
+
 }
--- backend/stock/src/main/java/com/sog/stock/domain/dto/StockDailyPriceResponseDTO.java
+++ backend/stock/src/main/java/com/sog/stock/domain/dto/StockDailyPriceResponseDTO.java
@@ -1,4 +1,18 @@
 package com.sog.stock.domain.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
 public class StockDailyPriceResponseDTO {
+
+    private String stockDate;
+    private Integer lowPrice;
+    private Integer highPrice;
+    private Integer startPrice;
+    private Integer endPrice;
+    private String prdyVrss; // 전일대비
+    private String prdyVrssSign; // 전일 대비 부호
+    private String prdyCtrt; // 전일 대비율
+
 }
