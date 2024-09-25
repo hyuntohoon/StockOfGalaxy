4a139ff78cd2bc2e55b8421d55ab2e8d76437fe3
차민주 <ckalswn007@naver.com>
2024-09-25 11:04:44 +0900

    feat: 행성 클러스터 모달 구현 완료

--- frontend/app/(route)/main/[date]/page.tsx
+++ frontend/app/(route)/main/[date]/page.tsx
@@ -1,95 +1,170 @@
 'use client';
 
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as THREE from 'three';
-import { RecoilRoot } from 'recoil';
+import { RecoilRoot, useRecoilValue } from 'recoil';
+import { dateState } from '@/app/store/date';
+import { HoveredPlanetData } from '@/app/types/main';
 import DateCard from '@/app/components/molecules/Card/DateCard';
 import TimeMachineButtonGroup from '@/app/components/molecules/ButtonGroup/TimeMachineButtonGroup';
+import PlanetTrendModal from '@/app/components/organisms/Modal/PlanetTrendModal';
+import { throttle } from 'lodash';
+import { useRouter } from 'next/navigation';
+
+const tempData = [
+  { stockCode: '100001', corpName: '삼성전자', value: 180 },
+  { stockCode: '100002', corpName: 'HLB', value: 165 },
+  { stockCode: '100003', corpName: '에코프로', value: 150 },
+  { stockCode: '100004', corpName: 'SK하이닉스', value: 135 },
+  { stockCode: '100005', corpName: '유한양행', value: 110 },
+  { stockCode: '100006', corpName: '유한양행', value: 95 },
+  { stockCode: '100007', corpName: '유한양행', value: 80 },
+  { stockCode: '100008', corpName: '유한양행', value: 65 },
+];
 
 export default function Page() {
+  const currentDate = useRecoilValue(dateState);
   const mountRef = useRef<HTMLDivElement>(null);
-
-  const tempData = [
-    { planetId: 1, value: 180 },
-    { planetId: 2, value: 165 },
-    { planetId: 3, value: 150 },
-    { planetId: 4, value: 135 },
-    { planetId: 5, value: 110 },
-    { planetId: 6, value: 95 },
-    { planetId: 7, value: 80 },
-    { planetId: 8, value: 65 },
-  ];
+  const [hoveredPlanet, setHoveredPlanet] = useState<HoveredPlanetData | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const camera = useRef<THREE.PerspectiveCamera>(new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000));
+  const router = useRouter();
 
   useEffect(() => {
     const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
-    camera.position.z = 550;
-
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor(0x000000, 0.0);
     mountRef.current?.appendChild(renderer.domElement);
 
-    const textureLoader = new THREE.TextureLoader();
+    const scene = new THREE.Scene();
+    camera.current.position.z = 550; // 카메라 위치 조정
+
     setupLights(scene);
     createStars(scene);
 
-    // 텍스처를 비동기적으로 미리 로드
+    const textureLoader = new THREE.TextureLoader();
     loadTextures(tempData, textureLoader).then((textures) => {
-      createPlanets(tempData, scene, textures, camera); // 모든 텍스처가 로드된 후에 행성 생성
+      createPlanets(tempData, scene, textures, camera.current);
     });
 
-    // 애니메이션 루프에서 프레임 속도 제한 (30 FPS)
-    const animate = () => {
-      setTimeout(() => {
-        requestAnimationFrame(animate); // 프레임 속도를 제한하여 CPU/GPU 부하를 줄임
-        renderer.render(scene, camera); // 장면을 렌더링
-      }, 1000 / 30); // 30 FPS로 제한 (1000ms를 30으로 나눈 값만큼 대기)
-    };
+    let frameId; // 애니메이션 프레임 ID 저장
+    function animate() {
+      frameId = requestAnimationFrame(animate); // 애니메이션 프레임 요청
+      renderer.render(scene, camera.current);
+    }
 
-    const handleResize = () => {
-      camera.aspect = window.innerWidth / window.innerHeight;
-      camera.updateProjectionMatrix();
+    function onWindowResize() {
+      camera.current.aspect = window.innerWidth / window.innerHeight;
+      camera.current.updateProjectionMatrix();
       renderer.setSize(window.innerWidth, window.innerHeight);
-    };
+    }
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener('resize', onWindowResize, false);
+    window.addEventListener('mousemove', event => onMouseMove(event, scene, renderer));
+    window.addEventListener('click', event => onPlanetClick(event, scene, renderer)); // 클릭 이벤트 추가
     animate();
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      cancelAnimationFrame(frameId); // 애니메이션 프레임 해제
+      window.removeEventListener('resize', onWindowResize);
+      window.removeEventListener('mousemove', event => onMouseMove(event, scene, renderer));
+      window.removeEventListener('click', event => onPlanetClick(event, scene, renderer)); // 클릭 이벤트 해제
       mountRef.current?.removeChild(renderer.domElement);
-      renderer.dispose();
     };
   }, []);
 
+  const onMouseMove = (event, scene, renderer) => {
+    const throttledMouseMove = throttle((event: MouseEvent) => {
+      const raycaster = new THREE.Raycaster();
+      const mouse = new THREE.Vector2(
+        (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
+        -(event.clientY / renderer.domElement.clientHeight) * 2 + 1
+      );
+
+      raycaster.setFromCamera(mouse, camera.current);
+      const intersects = raycaster.intersectObjects(scene.children);
+
+      if (intersects.length > 0) {
+        const intersected = intersects[0].object;
+        setHoveredPlanet({
+          stockCode: intersected.userData.stockCode,
+          corpName: intersected.userData.corpName,
+          position: intersected.position.clone(),
+        });
+        setIsModalOpen(true); // 모달 열기
+
+        // 커서를 포인터로 변경
+        document.body.style.cursor = 'pointer';
+      } else {
+        setHoveredPlanet(null);
+        setIsModalOpen(false); // 모달 닫기
+
+        // 커서를 기본으로 변경
+        document.body.style.cursor = 'auto';
+      }
+    }, 50); // 이벤트 처리 간격을 줄임
+
+    throttledMouseMove(event);
+  };
+
+  const onPlanetClick = (event, scene, renderer) => {
+    const raycaster = new THREE.Raycaster();
+    const mouse = new THREE.Vector2(
+      (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
+      -(event.clientY / renderer.domElement.clientHeight) * 2 + 1
+    );
+
+    raycaster.setFromCamera(mouse, camera.current);
+    const intersects = raycaster.intersectObjects(scene.children);
+
+    if (intersects.length > 0) {
+      const clickedPlanet = intersects[0].object;
+      const { stockCode } = clickedPlanet.userData;
+      // 클릭 시 해당 행성 메인 페이지로 이동
+      router.push(`/planet/main/${stockCode}/${currentDate}`);
+    }
+  };
+
   return (
-    <div ref={mountRef} id="canvas" style={{ width: '100%', height: '100vh', position: 'absolute', zIndex: 1 }}>
+    <div ref={mountRef} style={{ width: '100%', height: '100vh', position: 'absolute', zIndex: 1 }}>
       <RecoilRoot>
         <DateCard left="30px" />
+        {isModalOpen && hoveredPlanet && (
+          <PlanetTrendModal
+            stockCode={hoveredPlanet.stockCode}
+            corpName={hoveredPlanet.corpName}
+            position={hoveredPlanet.position}
+            camera={camera.current}
+            rendererDomElement={mountRef.current?.children[0] as HTMLCanvasElement}
+            onClose={() => setIsModalOpen(false)} // 모달 닫기 함수 전달
+          />
+        )}
       </RecoilRoot>
       <TimeMachineButtonGroup bottom="30px" right="20px" />
     </div>
   );
 }
 
-// 텍스처 미리 로드: 각 행성의 텍스처를 병렬로 로드하여 성능 최적화
 async function loadTextures(planetsData, textureLoader) {
   const promises = planetsData.map((data) => {
-    const textureId = data.planetId % 12 + 1; // todo: 플래닛 텍스쳐 불러오는 방법 수정 필요
+    const textureId = data.stockCode % 12 + 1;
     return new Promise((resolve) => {
-      textureLoader.load(`/image/${textureId}.jpg`, resolve); // 비동기적으로 텍스처 로드
+      textureLoader.load(`/images/planetTexture/${textureId}.jpg`, resolve);
     });
   });
-  return await Promise.all(promises); // 모든 텍스처가 로드된 후 반환
+  return await Promise.all(promises);
 }
 
 function createPlanets(planetsData, scene, textures, camera) {
   const centerPositions = [
-    { x: 0, y: 60 }, { x: -300, y: 120 }, { x: 180, y: -170 },
-    { x: -100, y: -220 }, { x: 250, y: 50 }, { x: 150, y: 250 },
-    { x: -280, y: -100 }, { x: -120, y: 270 },
+    { x: 0, y: 60, z: 0 },
+    { x: -300, y: 120, z: 0 },
+    { x: 180, y: -170, z: 0 },
+    { x: -100, y: -220, z: 0 },
+    { x: 250, y: 50, z: 0 },
+    { x: 150, y: 250, z: 0 },
+    { x: -280, y: -100, z: 0 },
+    { x: -120, y: 270, z: 0 },
   ];
 
   planetsData.forEach((data, index) => {
@@ -100,15 +175,13 @@ function createPlanets(planetsData, scene, textures, camera) {
     planet.position.set(
       centerPositions[index % centerPositions.length].x,
       centerPositions[index % centerPositions.length].y,
-      0
+      centerPositions[index % centerPositions.length].z,
     );
 
-    planet.lookAt(camera.position); // 카메라를 바라보도록 설정
     scene.add(planet);
+    planet.lookAt(camera.position);
 
-    // 메모리 관리: 사용된 geometry와 material을 명시적으로 해제
-    planet.geometry.dispose();
-    planet.material.dispose();
+    planet.userData = { stockCode: data.stockCode, corpName: data.corpName };
   });
 }
 
