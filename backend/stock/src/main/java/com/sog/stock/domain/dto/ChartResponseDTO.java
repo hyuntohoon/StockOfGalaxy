40bf2c4be18b3f3071e02d7207421f9f9e18c197
선예림 <syl1602@naver.com>
2024-09-26 11:22:50 +0900

    feat: #13. 실시간 chart ãresponse dto 작성

--- backend/stock/src/main/java/com/sog/stock/domain/dto/ChartResponseDTO.java
+++ backend/stock/src/main/java/com/sog/stock/domain/dto/ChartResponseDTO.java
@@ -1,4 +1,23 @@
 package com.sog.stock.domain.dto;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+import lombok.Data;
+
+@Data
 public class ChartResponseDTO {
+
+    // 날짜, 현재가, 종가, 저가, 고가, 누적거래량, 누적거래대금, 전일대비, 전일대비부호, 전일대비율
+    private String stockCode;  //종목코드
+    private LocalDateTime dailyStockHistoryDate; // 현재 ->
+    private String openPrice;
+    private String closePrice;
+    private String highPrice;
+    private String lowPrice;
+    private String stockAcmlVol;
+    private String stockAcmlTrPbmn;
+    private String prdyVrss;
+    private String prdyVrssSign;
+    private String prdyCtrt;
+
 }
