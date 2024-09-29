7a906f0dfee372964610a10bee1be3322b220214
sonbaejun <sonqowns2@naver.com>
2024-09-29 20:51:15 +0900

    fix : #20. TodayNewsResponseDTO 형식에 맞게 수정

--- backend/news/src/main/java/com/sog/news/domain/dto/TodayNewsResponseDTO.java
+++ backend/news/src/main/java/com/sog/news/domain/dto/TodayNewsResponseDTO.java
@@ -1,19 +1,26 @@
 package com.sog.news.domain.dto;
 
+import com.sog.news.global.NewsCategory;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class TodayNewsResponseDTO {
-    private Long id;
+    private Long newsId;
     private String title;
     private String content;
-    private String writeDate;  // 'write_date'는 자바에서 카멜케이스로 변경
-    private String newspaper;  // '신문사'는 영어로 변환
-    private String img;
+    private NewsCategory category;
+    private LocalDateTime publishedDate;
+    private String newsLink;
+    private double sentimentIndex;
+    private String thumbnailImg;
+    private LocalDateTime newsCreatedAt;
+    private LocalDateTime newsUpdatedAt;
 }
