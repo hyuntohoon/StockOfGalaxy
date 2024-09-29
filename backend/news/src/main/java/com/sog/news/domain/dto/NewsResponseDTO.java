79050f9fa79a4d6135bc324d2fb1a303a24340e5
sonbaejun <sonqowns2@naver.com>
2024-09-29 21:56:06 +0900

    fix : #20. NewsResponseDTO 형식에 맞게 수정

--- backend/news/src/main/java/com/sog/news/domain/dto/NewsResponseDTO.java
+++ backend/news/src/main/java/com/sog/news/domain/dto/NewsResponseDTO.java
@@ -1,22 +1,43 @@
 package com.sog.news.domain.dto;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import com.sog.news.domain.model.News;
+import com.sog.news.global.NewsCategory;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class NewsResponseDTO {
-    private Long id;
+    private Long newsId;
     private String title;
     private String content;
-    private LocalDateTime writeDate; // timestamp 처리
-    private String newspaper;  // 신문사
-    private String img;
-    private List<String> keywords;
+    private NewsCategory category;
+    private LocalDateTime publishedDate;
+    private String newsLink;
+    private double sentimentIndex;
+    private String thumbnailImg;
+    private LocalDateTime newsCreatedAt;
+    private LocalDateTime newsUpdatedAt;
+
+    // News 엔티티 -> DTO 변환
+    public static NewsResponseDTO fromEntity(News news) {
+        return NewsResponseDTO.builder()
+                .newsId(news.getNewsId())
+                .title(news.getTitle())
+                .content(news.getContent())
+                .category(news.getCategory())
+                .publishedDate(news.getPublishedDate())
+                .newsLink(news.getNewsLink())
+                .sentimentIndex(news.getSentimentIndex())
+                .thumbnailImg(news.getThumbnailImg())
+                .newsCreatedAt(news.getNewsCreatedAt())
+                .newsUpdatedAt(news.getNewsUpdatedAt())
+                .build();
+    }
 }
