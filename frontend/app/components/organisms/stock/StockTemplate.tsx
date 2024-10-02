d901129ac8f2a0d954f905ead410b09828f5688c
dltkdgus482 <dltkdgus482@naver.com>
2024-10-02 10:18:33 +0900

    feat: 현재가 조회 함수 추가
    [type]

--- frontend/app/components/organisms/stock/StockTemplate.tsx
+++ frontend/app/components/organisms/stock/StockTemplate.tsx
@@ -5,6 +5,7 @@ import styled from "@emotion/styled";
 import StockPrice from "../../molecules/stock/StockPrice";
 import StockInfo from "../../molecules/stock/StockInfo";
 import useKRStockWebSocket from "@/app/hooks/useKRStockWebSocket";
+import { getCurrentPrice } from "@/app/utils/apis/stock/getStockData";
 
 const ParentContainer = styled.div`
   width: 50vw;
@@ -182,6 +183,30 @@ const StockTemplate = () => {
       changeRate: null,
     },
   ]);
+
+  useEffect(() => {
+    stockDataInfo.map(async (stock, index) => {
+      try {
+        const res = await getCurrentPrice(stock.stock_code);
+
+        setStockDataInfo((prevStockData: any[]) => {
+          return prevStockData.map((stock) =>
+            stock.stock_code === res.stockCode
+              ? {
+                  stock_name: stock.stock_name,
+                  stock_code: res.stock,
+                  currentPrice: res.stckPrpr,
+                  changePrice: res.prdyVrss,
+                  changeRate: res.prdyCtrt,
+                }
+              : stock
+          );
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  }, []);
 
   useKRStockWebSocket(stockData, setStockDataInfo);
 
