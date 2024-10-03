9da242e34fee6d3a9978c8f3fa2a18f67c0d832f
차민주 <ckalswn007@naver.com>
2024-10-03 19:22:43 +0900

    feat: top8 주식 조회 api 추가

--- frontend/app/utils/apis/stock/index.ts
+++ frontend/app/utils/apis/stock/index.ts
@@ -10,3 +10,15 @@ export const getStockHistoryInfoApi = async (stockCode: string, date: string) =>
     throw error;
   }
 }
+
+// top8 종목 기사수와 순위 조회
+export const getPlanetTrendApi = async (date: string) => {
+  try {
+    const response = await defaultRequest.get(`/stock/top8/${date}`);
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.error("플래닛 트랜드 조회 실패", error);
+    throw error;
+  }
+}
