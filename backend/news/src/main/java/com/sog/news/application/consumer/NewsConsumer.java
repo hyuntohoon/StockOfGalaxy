74d80888807e425fb1a0e8611b11b61fe4abb16a
sonbaejun <sonqowns2@naver.com>
2024-10-03 14:33:01 +0900

    refactor : #26. NEWS 토픽 consumer DB INSERT 실패 상황 예외처리

--- backend/news/src/main/java/com/sog/news/application/consumer/NewsConsumer.java
+++ backend/news/src/main/java/com/sog/news/application/consumer/NewsConsumer.java
@@ -3,6 +3,8 @@ package com.sog.news.application.consumer;
 import com.sog.news.domain.model.News;
 import com.sog.news.domain.repository.NewsRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
@@ -12,10 +14,17 @@ import org.springframework.stereotype.Service;
 public class NewsConsumer {
 
     private final NewsRepository newsRepository;
+    private static final Logger logger = LoggerFactory.getLogger(NewsConsumer.class);
 
-    @KafkaListener(topics = "NEWS-TEST", groupId = "testNews", containerFactory = "newsKafkaListenerContainerFactory")
-    public void processGameResults(News news, Acknowledgment ack) {
-        System.out.println("news.toString() = " + news.toString());
-        newsRepository.save(news);
+    @KafkaListener(topics = "NEWS-TEST-LOCAL7", groupId = "testNews", containerFactory = "newsKafkaListenerContainerFactory")
+    public void consumeNewsMessage(News news, Acknowledgment ack) {
+        try {
+            if (news.getCategory() != null) { // category ENUM 타입 매핑 실패 대비
+                newsRepository.save(news);
+                ack.acknowledge(); // 성공적으로 INSERT되면 ACK 전송
+            }
+        } catch (Exception e) {
+            logger.error("DB INSERT 실패: {}", e.getMessage()); // ACK을 호출하지 않으므로 해당 메시지는 버려짐
+        }
     }
 }
