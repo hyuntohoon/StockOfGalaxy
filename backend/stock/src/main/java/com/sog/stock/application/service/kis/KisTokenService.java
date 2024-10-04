e5a8148b24e9feac782dc7765a66c016b10a45a5
선예림 <syl1602@naver.com>
2024-10-04 15:49:54 +0900

    fix: token, websocket key ì저장 ì시간 늘리기

--- backend/stock/src/main/java/com/sog/stock/application/service/kis/KisTokenService.java
+++ backend/stock/src/main/java/com/sog/stock/application/service/kis/KisTokenService.java
@@ -75,7 +75,7 @@ public class KisTokenService {
                 if (response.getAccessToken() == null) {
                     return Mono.error(new NullPointerException("AccessToken is null"));
                 }
-                redisService.setValues(TOKEN_KEY, response.getAccessToken(), Duration.ofHours(6));
+                redisService.setValues(TOKEN_KEY, response.getAccessToken(), Duration.ofHours(24));
                 return Mono.just(response.getAccessToken());
             });
 
