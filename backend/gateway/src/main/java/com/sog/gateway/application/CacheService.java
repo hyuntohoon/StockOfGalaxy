f68748b29dfa76fb3eaa11b4bee5d47a3b33edca
박지훈 <zlxlgus123@naver.com>
2024-10-04 15:23:18 +0900

    fix #28 : object to string

--- backend/gateway/src/main/java/com/sog/gateway/application/CacheService.java
+++ backend/gateway/src/main/java/com/sog/gateway/application/CacheService.java
@@ -6,15 +6,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CacheService {
-    @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
 
-    public Object getCache(String key) {
-        return redisTemplate.opsForValue().get(key);
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    public String getCache(String key) {
+        return redisTemplate.opsForValue().get(key); // JSON 문자열로 저장
     }
 
-    public void setCache(String key, Object value) {
-        redisTemplate.opsForValue().set(key, value);
+    public void setCache(String key, String value) {
+        redisTemplate.opsForValue().set(key, value); // JSON 문자열을 저장
     }
 
     public boolean hasKey(String key) {
