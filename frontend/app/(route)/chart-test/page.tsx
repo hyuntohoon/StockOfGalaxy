881a699b73a2d12541e13c10cb4e6891eb8b8ad0
dltkdgus482 <dltkdgus482@naver.com>
2024-10-05 14:01:33 +0900

    feat: 프론트 에러 로깅 라이브러리 추가
    [type]

--- frontend/app/(route)/chart-test/page.tsx
+++ frontend/app/(route)/chart-test/page.tsx
@@ -3,14 +3,24 @@
 import FinancialMetricsChart from "@/app/components/molecules/stock/FinancialMetricsChart";
 import { ErrorBoundary } from "react-error-boundary";
 
-const ChartPage = () => {
-  const logError = (error: Error, info: { componentStack: string }) => {
-    console.log(error);
-    console.log(info);
-  };
+const logError = (error: Error, info: { componentStack: string }) => {
+  console.log("Error:", error);
+  console.log("Info:", info);
+};
 
+const FallbackComponent = ({ error, resetErrorBoundary }: any) => {
   return (
-    <ErrorBoundary fallback={<div>Error</div>} onError={logError}>
+    <div>
+      <p>Something went wrong:</p>
+      <pre>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  );
+};
+
+const ChartPage = () => {
+  return (
+    <ErrorBoundary FallbackComponent={FallbackComponent} onError={logError}>
       <FinancialMetricsChart />
     </ErrorBoundary>
   );
