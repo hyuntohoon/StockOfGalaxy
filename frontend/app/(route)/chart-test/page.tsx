72ba536f5faa41c287cf04034b2ff86157681750
dltkdgus482 <dltkdgus482@naver.com>
2024-10-05 13:52:58 +0900

    feat: 에러 바운더리 라이브러리 추가
    [type]

--- frontend/app/(route)/chart-test/page.tsx
+++ frontend/app/(route)/chart-test/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import FinancialMetricsChart from "@/app/components/molecules/stock/FinancialMetricsChart";
-import ErrorBoundary from "@/app/components/templates/ErrorBoundary";
+import { ErrorBoundary } from "react-error-boundary";
 
 const ChartPage = () => {
+  const logError = (error: Error, info: { componentStack: string }) => {
+    console.log(error);
+    console.log(info);
+  };
+
   return (
-    <ErrorBoundary>
+    <ErrorBoundary fallback={<div>Error</div>} onError={logError}>
       <FinancialMetricsChart />
     </ErrorBoundary>
   );
