90edf7c89c10c8a53aca2b8c07c12fed02c89f32
차민주 <ckalswn007@naver.com>
2024-10-08 22:34:55 +0900

    feat: 오늘로 이동하기 alert 대신 확인 모달 추가

--- frontend/app/components/molecules/ButtonGroup/Header/ReturnTodayButtonGroup.tsx
+++ frontend/app/components/molecules/ButtonGroup/Header/ReturnTodayButtonGroup.tsx
@@ -1,28 +1,22 @@
 import { BsCalendar2CheckFill } from 'react-icons/bs';
 import HeaderButtonGroup from '../../HeaderButtonGroup';
-import { useDate } from '@/app/store/date';
-import { useRouter } from 'next/navigation'; // next/navigation에서 useRouter 가져오기
 
-const ReturnTodayButtonGroup = () => {
-  const {setDate} = useDate();
-  const router = useRouter(); // useRouter를 import하여 router 객체를 받아오기
+interface ReturnTodayButtonGroupProps {
+  openModal: () => void;
+}
 
+const ReturnTodayButtonGroup: React.FC<ReturnTodayButtonGroupProps> = ({ openModal }) => {
   const handleClick = () => {
-    const today = new Date(); // 로컬 시간으로 오늘 날짜 가져오기
-  
-    // 오늘 날짜를 'YYYYMMDD' 형식으로 변환
-    const formattedDate = today.toISOString().slice(0, 10).replace(/-/g, ''); // 'YYYYMMDD' 형식으로 변환
-  
-    setDate(formattedDate); // 날짜 설정
-    alert("오늘 날짜로 이동합니다");
-    router.push(`/main/${formattedDate}`); // 오늘 날짜로 이동
+    openModal();  // 모달 열기
   };
+
   return (
     <div onClick={handleClick}>
-    <HeaderButtonGroup
-      icon={<BsCalendar2CheckFill />}
-      label="오늘로 돌아가기"
-    /></div>
+      <HeaderButtonGroup
+        icon={<BsCalendar2CheckFill />}
+        label="오늘로 돌아가기"
+      />
+    </div>
   );
 };
 
