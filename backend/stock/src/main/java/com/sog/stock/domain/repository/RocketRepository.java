943b525d0c3540d753d87b40f80bff62272ffd3d
선예림 <syl1602@naver.com>
2024-10-09 14:27:08 +0900

    fix: 로켓 정렬시 순서 유지 ì안되는 현상 flatMap -> concatMap 사용하여 수정

--- backend/stock/src/main/java/com/sog/stock/domain/repository/RocketRepository.java
+++ backend/stock/src/main/java/com/sog/stock/domain/repository/RocketRepository.java
@@ -11,12 +11,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RocketRepository extends JpaRepository<Rocket, Integer> {
 
-    // 주어진 Stock 엔티티와 연결된 모든 로켓을 등록된 시간 기준 오름차순으로 조회
-    @Query(value = "SELECT * FROM rocket WHERE stock_code = :stockCode AND is_deleted = false ORDER BY rocket_created_at DESC, rocket_id ASC", nativeQuery = true)
-    List<Rocket> findAllByStockOrderByCreatedAtDesc(@Param("stockCode") String stockCode);
+    @Query(value = "SELECT * FROM rocket WHERE stock_code = :stockCode AND is_deleted = false ORDER BY rocket_id DESC", nativeQuery = true)
+    List<Rocket> findAllByStockRAndRocketIdDesc(@Param("stockCode") String stockCode);
 
     // Stock 엔티티에 해당하는 로켓을 PK 내림차순으로 상위 7개만 조회
-    @Query(value = "SELECT * FROM rocket WHERE stock_code = :stockCode AND is_deleted = false ORDER BY rocket_created_at DESC, rocket_id ASC LIMIT 7", nativeQuery = true)
+    @Query(value = "SELECT * FROM rocket WHERE stock_code = :stockCode AND is_deleted = false ORDER BY rocket_id DESC, rocket_id ASC LIMIT 7", nativeQuery = true)
     List<Rocket> findTop7ByStock(@Param("stockCode") String stockCode);
 
 }
