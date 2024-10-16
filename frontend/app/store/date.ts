e23af853499dcd62bbed802a14c80ee2abcfd7b9
dltkdgus482 <dltkdgus482@naver.com>
2024-10-16 23:26:50 +0900

    fix: 발표 시연을 위해 today 날짜 임의로 수정
    [type]

--- frontend/app/store/date.ts
+++ frontend/app/store/date.ts
@@ -1,12 +1,11 @@
-import { atom, SetterOrUpdater } from 'recoil';
-import { getTodayDate } from '../utils/libs/getTodayDate';
-import { useRecoilState } from 'recoil';
+import { atom, SetterOrUpdater } from "recoil";
+import { getTodayDate } from "../utils/libs/getTodayDate";
+import { useRecoilState } from "recoil";
 
 export const dateState = atom({
-  key: 'dateState', // 고유 key 값
-  default: "20241008", // default 값은 오늘 날짜
+  key: "dateState", // 고유 key 값
+  default: getTodayDate(), // default 값은 오늘 날짜
 });
-
 
 // // newDate: yyyymmdd 형식으로 (ex. 20240916)
 // // 날짜 변경 함수
@@ -16,9 +15,9 @@ export const dateState = atom({
 
 export const useDate = () => {
   const [date, setDate] = useRecoilState(dateState);
-  const today = "20241008";
+  const today = getTodayDate();
 
   // 현재 date가 오늘 날짜인지 확인
   const isToday = date === today;
-  return { date, setDate, isToday};
-}
+  return { date, setDate, isToday };
+};
